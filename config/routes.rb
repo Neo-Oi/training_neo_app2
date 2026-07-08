4597733c70ec8e3a5b98c56290e664129b3d1906
Neo-Oi <neo-oi@comthink.co.jp>
2026-07-08 11:10:32 +0900

    ログインを独立させて新規作成時のバリデーションと被らないように変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
  get 'homes',to: 'homes#index'
  get 'fails',to: 'fails#index'
  get 'logins',to: 'logins#index'
- get 'logins/new',to: 'logins#new'
  post 'logins',to: 'logins#create'
  post "/reset_login_fails", to: "fails#reset_login_fails"
  delete "/logout", to: "logins#destroy", as: :logout
